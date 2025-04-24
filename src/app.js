2d7256f81a3343922201572d5174e91c5c8c26ed
fathironmy2 <fathironmy2@gmail.com>
2025-04-24 12:39:35 +0000

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1553915142.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1793178327.
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const fs = require('fs');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -15,10 +18,19 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    fs.readFile('./src/database/blog.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Failed to read data file' });
+          return;
+        }
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
   });
+
 });
 
 app.use('/api/v1', api);
